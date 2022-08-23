3cb59ff68cc57a8933b9de4f8fae93f2135c0ed0
przemq20 <lechowiczprzemyslaw@gmai.com>
2022-08-23 18:52:07 +0200

    Updated port

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,11 +7,12 @@ import http.Router
 import scala.concurrent.ExecutionContext
 
 object Main extends App {
-  implicit val system:           ActorSystem[Any] = ActorSystem(Behaviors.empty, "SingleRequest")
+  implicit val system: ActorSystem[Any] = ActorSystem(Behaviors.empty, "SingleRequest")
   implicit val executionContext: ExecutionContext = system.executionContext
 
-  val api    = new ApiConnector()
+  val api = new ApiConnector()
   val router = new Router(api)
+  val port = if (System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
 
-  val serverSource = Http().newServerAt("localhost", 8080).bind(router.routes)
+  val serverSource = Http().newServerAt("localhost", port).bind(router.routes)
 }
